c7c7844073a41f598a622ac7a9b57f48e3edcb07
Kris001Sharma <krish001.sharma@gmail.com>
2021-06-06 10:36:02 +0530

    Update PremiumVsFreemium.sql
    shortened the code

--- PremiumVsFreemium.sql
+++ PremiumVsFreemium.sql
@@ -6,22 +6,11 @@ from ms_acc_dimension ad
 inner join ms_user_dimension ud on ud.acc_id = ad.acc_id
 left join ms_download_facts df on df.user_id = ud.user_id
 group by ad.paying_customer, df.date
---order by  df.date
-),
-cteNo as(
-select date, download
-from cte
-where paying_customer = 'no'
-),
-cteYes as(
-select date, download
-from cte
-where paying_customer = 'yes'
 )
 select y.date, n.download as non_paying, y.download as paying
-from cteNo n
-inner join cteYes y on y.date = n.date
-where n.download > y.download
+from cte n
+inner join cte y on y.date = n.date
+where n.download > y.download and n.paying_customer = 'no' and y.paying_customer = 'yes'
 order by y.date
 
 

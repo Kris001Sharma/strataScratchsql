a80d16b19f3ed76d04cc7fe5250f161e291999e6
Kris001Sharma <krish001.sharma@gmail.com>
2021-06-06 10:44:31 +0530

    Update PremiumVsFreemium.sql

--- PremiumVsFreemium.sql
+++ PremiumVsFreemium.sql
@@ -1,16 +1,16 @@
 with cte
 as(
-select ad.paying_customer, 
- df.date, sum(df.downloads) as download
+select  df.date,
+sum(case when ad.paying_customer = 'yes' then df.downloads end) as paying_downloads,
+sum(case when ad.paying_customer = 'no' then df.downloads end) as non_paying_downloads
 from ms_acc_dimension ad
 inner join ms_user_dimension ud on ud.acc_id = ad.acc_id
 left join ms_download_facts df on df.user_id = ud.user_id
-group by ad.paying_customer, df.date
+group by df.date
 )
-select y.date, n.download as non_paying, y.download as paying
+select date, non_paying_downloads, paying_downloads
 from cte n
-inner join cte y on y.date = n.date
-where n.download > y.download and n.paying_customer = 'no' and y.paying_customer = 'yes'
-order by y.date
+where non_paying_downloads > paying_downloads
+order by date
 
 
